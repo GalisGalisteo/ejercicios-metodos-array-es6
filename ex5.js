8390ee28482fea8a42e833daf5b6d61ae210feaf
GalisGalisteo <galis.travelling@gmail.com>
2023-11-06 17:35:06 +0100

    ex5 finished

--- ex5.js
+++ ex5.js
@@ -3,14 +3,16 @@
 // https://www.w3schools.com/react/react_es6_spread.asp
 
 let paciente = {
-    nombre: "Pedro",
-    ultimaVisitaPagada: false,
-    ultimaVisita: '4/4/2022 18: 32: 19'
-}
+  nombre: "Pedro",
+  ultimaVisitaPagada: false,
+  ultimaVisita: "4/4/2022 18: 32: 19",
+};
 
 // A) COPIA el paciente usando el operador de spread a un NUEVO objeto de nombre 'pacienteCopiado'; pero añade una nueva propiedad que se llame 'localidad'; y asignale cualquier valor
 
-// console.log(pacienteCopiado) ;
+let pacienteCopiado = { ...paciente, localidad: "barcelona" };
+
+console.log(pacienteCopiado);
 /**
  * {
     nombre: "Pedro",
@@ -20,22 +22,33 @@ let paciente = {
 }
  */
 
-// console.log(paciente == pacienteCopiado); // false 
+console.log(paciente == pacienteCopiado); // false
 // IMPORTANTE^ : Los objetos deben estar en diferentes posiciones de memoria, la comparación debe dar false
 
-let pacientes = [{
+let pacientes = [
+  {
     nombre: "Pedro",
     ultimaVisitaPagada: false,
-    ultimaVisita: '4/4/2022 18: 32: 19'
-}, {
+    ultimaVisita: "4/4/2022 18: 32: 19",
+  },
+  {
     nombre: "María",
     ultimaVisitaPagada: true,
-    ultimaVisita: '4/4/2022 18: 32: 19'
-}];
+    ultimaVisita: "4/4/2022 18: 32: 19",
+  },
+];
 
 // B) COPIA el array de pacientes Y AÑADE un nuevo paciente. USa el operador de 'spread' para realizar la copia, NO HAGAS PUSH. Guarda el array nuevo en una variable de nombre 'pacientesActualizados'
 
-// console.log(pacientesActualizados)
+const pacienteNuevo = {
+  nombre: "Olivia",
+  ultimaVisitaPagada: false,
+  ultimaVisita: "1/4/2021 18: 32: 19",
+};
+
+const pacientesActualizados = [...pacientes, pacienteNuevo];
+
+console.log(pacientesActualizados);
 
 /**
  * let pacientes = [{
@@ -53,4 +66,4 @@ let pacientes = [{
 }];
  */
 
-// console.log(pacientes == pacientesActualizados); // false
+console.log(pacientes == pacientesActualizados); // false
